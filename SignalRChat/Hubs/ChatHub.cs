bb558e5032821112e8a6e4b2e6974ff58d547b1e
sephirothwzc <zhanchao.wu@icloud.com>
2015-08-08 14:50:46 +0800

    2015年8月8日14:50:35 吴占�

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
@@ -20,6 +21,19 @@ namespace SignalRChat.Hubs
         {
             // Call the addNewMessageToPage method to update clients.
             Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public override Task OnConnected()
+        {
+            //Program.MainForm.WriteToConsole("Client connected: " + Context.ConnectionId);
+            Clients.All.addNewMessageToPage("SYSTEM", Context.ConnectionId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Clients.All.addNewMessageToPage("SYSTEM", Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
         }
     }
 }
