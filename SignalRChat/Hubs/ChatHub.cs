d8cee201f9701ed3f3e565ecb535a634624fb2a7
sephirothwzc <zhanchao.wu@icloud.com>
2015-08-08 13:36:20 +0800

    2015年8月8日13:36:04 吴占超创建

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    /// <summary>
+    /// You will use this class as a SignalR server hub that sends messages to all clients
+    /// </summary>
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+
+namespace SignalRChat
+{
+    /// <summary>
+    /// Change the contents of the file to the following
+    /// </summary>
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
